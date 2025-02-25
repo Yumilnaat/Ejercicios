30e4097a45f4e2bb8309fa957abe4d6cd04959b8
Yumilnaat <yumilnaat031225@gmail.com>
2025-02-25 16:53:46 -0500

    Add files via upload

---
+++ Ejercicios 1.2/3gestorPaquetes/index.js
@@ -0,0 +1,11 @@
+/*
+var generateName = require('sillyname');
+var sillyName = generateName();
+
+console.log(sillyName);
+*/
+
+import superheroes, {randomSuperhero} from 'superheroes';
+
+randomSuperhero();
+console.log(randomSuperhero());
