f8096309d7dead28228f3fdc94db4cf058047be5
Yumilnaat <yumilnaat031225@gmail.com>
2025-05-14 00:50:21 -0500

    Add files via upload

--- 3.2 API CRUD/index.js
+++ 3.2 API CRUD/index.js
@@ -26,6 +26,63 @@ app.post('/usuarios', async (req, res) => {
     }
 });
 
+app.put('/usuarios/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const usuario = await Usuario.findByIdAndUpdate(id, req.body);
+        if (!usuario) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+        const usuarioActualizado = await Usuario.findById(id);
+        res.status(200).json(usuarioActualizado);
+        console.log(usuarioActualizado);
+    } catch (error) {
+        console.error('Error al actualizar el usuario:', error);
+        res.status(500).json({ error: 'Error al actualizar el usuario' });
+    }
+});
+
+app.delete('/usuarios/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const usuario = await Usuario.findByIdAndDelete(id);
+        if (!usuario) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+        res.status(200).json({ message: 'Usuario eliminado' });
+    } catch (error) {
+        console.error('Error al eliminar el usuario:', error);
+        res.status(500).json({ error: 'Error al eliminar el usuario' });
+    }
+});
+
+app.get('/usuarios', async (req, res) => {
+    try {
+        const usuarios = await Usuario.find();
+        res.status(200).json(usuarios);
+    } catch (error) {
+        console.error('Error al obtener los usuarios:', error);
+        res.status(500).json({ error: 'Error al obtener los usuarios' });
+    }
+}
+);
+
+app.get('/usuarios/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const usuarios = await Usuario.findById(id);
+        if (!usuarios) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+
+        res.status(200).json(usuarios);
+    } catch (error) {
+        console.error('Error al obtener los usuarios:', error);
+        res.status(500).json({ error: 'Error al obtener los usuarios' });
+    }
+}
+);
+
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
